802dae01c19a050d5d4883ea17b57ec9b1e050d3
admin <togerthe@naver.com>
2022-01-27 18:10:32 +0900

    indices,currencies,futures 시작-2(모델링코드)

--- 업무 분담.py
+++ 업무 분담.py
@@ -1,12 +1,42 @@
-< 업무분담 - 2 >
-예측 할 컬럼 4개 : Low , High, Adj Close, Change
-1. 마지막 벡테스팅용 30개 데이터는 따로 만들기 (예시 : last_30_currency )
-1. minmaxscaler 만들기( 이름 예시 : currency_minmaxscaler)
-2. 30개씩 예측시키기 - train_test_split까지(test_size = 0.2) for문돌릴 때, 전체행 갯수 -30
-3. 모델링은 갑래님 모델과 같은 구조로
-4. minmaxscaler, 모델.h5, train_test_split, last_30개 데이터를 만들 때 저장이름
-   : 자기파트 섹터_model.h5, 자기파트 섹터_last_30 등  (예시 : futures_minmaxscaler)
-코랩과 파이참을 이용해서 ....!
+<업무 분담3>
+각자 모델링한 파트 * 4개의 열(Low , High, Adj Close, Change)
+ex) 종가의(Adj Close) 경우
+
+
+각 last_30 데이터프레임에 추가시키고(어제 종가까지의 데이터가 있음)
+다시 마지막 30개의 데이터를 예측시켜서 표에다가 입력
+다음날 XX:XX시에 실제 값과 비교하여
+24개까지 예측하고 난 후에 정답을 매김
+각 모델의 정답률을 보여주고
+정답률이 80프로 이상인 모델은(종목-열) 따로 '유망종목' 등등의 이름으로 보여주기.
+
+'실행' 하면 기존의 Df마지막 날 이후부터(end_day + today) 어제까지의 종가데이터를 하져옴
+ * 문제점: Yahoo Finance의 데이터 업데이터 시간이 정확한가..
+
+ex) High의 경우
+ex) Low 의 경우
+ex) Change 의 경우
+
+* low high close는 정답지표를 어떤 기준으로 할지... 흠
+
+
+
+
+
+
+
+
+
+
+# < 업무분담 - 2 >
+# 예측 할 컬럼 4개 : Low , High, Adj Close, Change
+# 1. 마지막 벡테스팅용 30개 데이터는 따로 만들기 (예시 : last_30_currency )
+# 1. minmaxscaler 만들기( 이름 예시 : currency_minmaxscaler)
+# 2. 30개씩 예측시키기 - train_test_split까지(test_size = 0.2) for문돌릴 때, 전체행 갯수 -30
+# 3. 모델링은 갑래님 모델과 같은 구조로
+# 4. minmaxscaler, 모델.h5, train_test_split, last_30개 데이터를 만들 때 저장이름
+#    : 자기파트 섹터_model.h5, 자기파트 섹터_last_30 등  (예시 : futures_minmaxscaler)
+# 코랩과 파이참을 이용해서 ....!
 
 
 
